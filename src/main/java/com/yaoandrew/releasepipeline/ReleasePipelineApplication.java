27dce09bde08d07327119d0cc5583851bceb1bd6
Andrew J Yao <1547809+yaoandrew@users.noreply.github.com>
2023-11-29 12:45:23 -0800

    Initial commit

---
+++ src/main/java/com/yaoandrew/releasepipeline/ReleasePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.yaoandrew.releasepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasePipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaoandrew/releasepipeline/ReleasePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaoandrew.releasepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleasePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
